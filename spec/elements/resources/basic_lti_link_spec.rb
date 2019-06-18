954a4c0aa3fd940e96677dba5c327a3fec4b3bac
Remy Obein <remy@cassia.tech>
2019-06-18 20:08:45 +0000

    specs: Fix basic lti link spec.

--- spec/elements/resources/basic_lti_link_spec.rb
+++ spec/elements/resources/basic_lti_link_spec.rb
@@ -3,21 +3,23 @@ require 'spec_helper'
 module CommonCartridge
   module Elements
     module Resources
-      describe BasicLTILink do
+      describe BasicLtiLink do
         before(:all) do
           manifest = CommonCartridge.parse_from_zip('canvas_small_1.1.imscc').manifest
           @resource = manifest.resources.detect { |r| r.identifier =~ /I_00015_R/ }
         end
 
-        it "is parsed correctly" do
-          expect(@resource.files.first.content).to be_kind_of(CommonCartridge::Elements::Resources::BasicLTILink)
+        it 'is parsed correctly' do
+          expect(@resource.files.first.content).to be_kind_of(
+            CommonCartridge::Elements::Resources::BasicLtiLink::BasicLtiLink
+          )
         end
 
-        it "has a title" do
+        it 'has a title' do
           expect(@resource.files.first.content.title).to eq('Semester 1 Overview')
         end
 
-        it "has a secure_launch_url" do
+        it 'has a secure_launch_url' do
           expect(@resource.files.first.content.secure_launch_url).to eq('https://fakecollege.com/activity_lists/student-resources--122/activities/semester-1-overview--17?custom_course_id=275')
         end
       end
